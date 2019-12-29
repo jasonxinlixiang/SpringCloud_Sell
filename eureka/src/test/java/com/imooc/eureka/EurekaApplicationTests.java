df9bf89c4956636970b0bcbc0526b384ed7b67e6
向心力 <>
2019-12-29 23:11:30 +0800

    3-1 - Create eureka server project

---
+++ eureka/src/test/java/com/imooc/eureka/EurekaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.imooc.eureka;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

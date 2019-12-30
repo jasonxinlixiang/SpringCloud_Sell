9b89c982434aaeae85823583da454d71cfbbdb71
向心力 <>
2019-12-30 16:08:05 +0800

    4-10 - Create OrderService

---
+++ order/src/main/java/com/imooc/order/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.imooc.order.controller;
+
+public class OrderController {
+
+    /**
+     * 1. 参数校验
+     * 2. 查询商品信息
+     * 3. 计算总价
+     * 4. 扣库存（调用商品信息）
+     * 4. 订单入库
+     */
+    public void create(){
+
+    }
+}

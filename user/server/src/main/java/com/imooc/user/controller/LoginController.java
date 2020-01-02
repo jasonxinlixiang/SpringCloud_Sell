dab319eda9388ab83f0c747cd7001f729d688527
向心力 <>
2020-01-02 14:20:51 +0800

    9-5 - Finish seller oauth2 function in user project

--- user/server/src/main/java/com/imooc/user/controller/LoginController.java
+++ user/server/src/main/java/com/imooc/user/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.imooc.user.controller;
 
 import com.imooc.user.VO.ResultVO;
 import com.imooc.user.constant.CookieConstant;
+import com.imooc.user.constant.RedisConstant;
 import com.imooc.user.dataobject.UserInfo;
 import com.imooc.user.enums.ResultEnum;
 import com.imooc.user.enums.RoleEnum;
@@ -9,12 +10,18 @@ import com.imooc.user.service.UserService;
 import com.imooc.user.utils.CookieUtil;
 import com.imooc.user.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/login")
@@ -22,6 +29,9 @@ public class LoginController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 买家登录
@@ -57,7 +67,38 @@ public class LoginController {
      */
     @GetMapping("/seller")
     public ResultVO seller(@RequestParam("openid") String openid,
+                          HttpServletRequest request,
                           HttpServletResponse response){
-        return null;
+        // 判断是否登录
+        Cookie cookie = CookieUtil.get(request, CookieConstant.TOKEN);
+        // 从redis中取出cookie的值
+        if (cookie != null &&
+                !StringUtils.isEmpty(stringRedisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_TEMPLATE, cookie.getValue())))){
+            return ResultVOUtil.success();
+        }
+
+        //1. openid和数据库中的数据匹配
+        UserInfo userInfo = userService.findByOpenid(openid);
+        if (userInfo == null){
+            return ResultVOUtil.error(ResultEnum.LOGIN_FAIL);
+        }
+
+        //2. 判断角色
+        if (!RoleEnum.SELLER.getCode().equals(userInfo.getRole())){
+            return ResultVOUtil.error(ResultEnum.ROLE_ERROR);
+        }
+
+        //3. redis设置key=UUID, value=xyz
+        String token = UUID.randomUUID().toString();
+        Integer expire = CookieConstant.expire;
+        stringRedisTemplate.opsForValue().set(String.format(RedisConstant.TOKEN_TEMPLATE, token),
+                openid,
+                expire,
+                TimeUnit.SECONDS);
+
+        //4. cookie里设置openid=abc
+        CookieUtil.set(response, CookieConstant.TOKEN, token, CookieConstant.expire);
+
+        return ResultVOUtil.success();
     }
 }

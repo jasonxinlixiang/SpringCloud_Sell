2ce674d1680488ffb60abf4649e39b6223607305
向心力 <>
2020-01-02 16:07:48 +0800

    9-9 - Cross origin

--- api-gateway/src/main/java/com/imooc/apigateway/filter/AuthBuyerFilter.java
+++ api-gateway/src/main/java/com/imooc/apigateway/filter/AuthBuyerFilter.java
@@ -1,10 +1,16 @@
 package com.imooc.apigateway.filter;
 
+import com.imooc.apigateway.constant.RedisConstant;
+import com.imooc.apigateway.utils.CookieUtil;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
@@ -14,7 +20,11 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
  * 权限拦截（区分买家和卖家）
  */
 @Component
-public class OauthFilter extends ZuulFilter {
+public class AuthBuyerFilter extends ZuulFilter {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public String filterType() {
         return PRE_TYPE;
@@ -27,6 +37,11 @@ public class OauthFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        if ("/order/order/create".equals(request.getRequestURI())){
+            return true;
+        }
         return true;
     }
 
@@ -36,10 +51,15 @@ public class OauthFilter extends ZuulFilter {
         HttpServletRequest request = requestContext.getRequest();
 
         /**
-         * /order/create 只能买家访问
-         * /order/finish 只能卖家访问
-         * /product/list 都可访问
+         * /order/create 只能买家访问(cookie里有openid)
          */
+
+        Cookie cookie = CookieUtil.get(request, "openid");
+        if (cookie == null || StringUtils.isEmpty(cookie.getValue())) {
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+
         return null;
     }
 }

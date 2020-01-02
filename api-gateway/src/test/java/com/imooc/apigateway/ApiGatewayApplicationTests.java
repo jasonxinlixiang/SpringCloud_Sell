524296baef38062898c2764a6636830a56f01571
向心力 <>
2020-01-02 10:09:58 +0800

    8-2 - Zuul routes

---
+++ api-gateway/src/test/java/com/imooc/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+//package com.imooc.apigateway;
+//
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiGatewayApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

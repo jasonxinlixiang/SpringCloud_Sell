83f28cc4f011f87452442d3dab4d744771ab85cc
向心力 <>
2020-01-02 20:38:14 +0800

    10-4 - Circuit Breaker mode

--- order/server/src/main/java/com/imooc/order/controller/HystrixController.java
+++ order/server/src/main/java/com/imooc/order/controller/HystrixController.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,11 +15,21 @@ import java.util.Arrays;
 public class HystrixController {
 
     //@HystrixCommand(fallbackMethod = "fallback")
+    //超时配置
+//    @HystrixCommand(commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+//    })
     @HystrixCommand(commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),  				//设置熔断
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),	//请求数达到后才计算
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"), //休眠时间窗
+			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),	//错误率
     })
     @GetMapping("/getProductInfoList")
-    public String getProductInfoList(){
+    public String getProductInfoList(@RequestParam("number") Integer number){
+        if (number % 2 == 0){
+            return "success";
+        }
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject("http://127.0.0.1:8005/product/listForOrder",
                 Arrays.asList("157875196366160022"),
